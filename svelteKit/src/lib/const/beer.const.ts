526f5fb5fe9dc4e026795ca6eac3ddb8fe4714ad
onsato <nogiglee@gmail.com>
2025-02-12 23:03:28 +0900

    構成作り直し

--- svelteKit/src/lib/const/beer.const.ts
+++
@@ -1,5 +0,0 @@
-type Beer = {
-  name: string;
-  ibu: number; // 苦味 (0-100)
-  fruitiness: number; // フルーティさ (0-100)
-};
--- svelteKit/src/routes/my-page/+page.ts
+++
@@ -1,6 +0,0 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const ssr = true;
-export const csr = true;
-
-export const prerender = false;
